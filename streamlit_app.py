074533e90b9cc7b57b1744c13d6fa492f5fe3e3b
MathieuGrainger <Mathieu.Grainger@health.wa.gov.au>
2024-06-05 14:38:35 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ from snowflake.snowpark.session import Session
 from snowflake.snowpark import functions as F
 from snowflake.snowpark.types import *
 from snowflake.snowpark import Window
-
 from io import StringIO
+
 import streamlit as st
 import pandas as pd
 import json
